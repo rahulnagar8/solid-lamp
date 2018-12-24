6e6034ac380fdc2515edbd5819a9f63340998275
NAMRATA SWAIN <namrataswain456@gmail.com>
2018-12-24 21:09:30 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,10 +1,10 @@
 class fruits
 {private String name="Apple";
 private int price;
- public float profit;
+
  private void show(int price, float profit)
  {this.price=price;
-  this.profit=profit;
+  
  }
 public static void main(String [] args)
 {System.out.println(name);
